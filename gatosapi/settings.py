09d8d726166eb92cfc1db2c96d122aee5cfb99d5
Jazael Galindo <115960860+JazaeloG@users.noreply.github.com>
2023-06-01 09:36:55 -0600

    Update settings.py

--- gatosapi/settings.py
+++ gatosapi/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'health_check',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
